76e8c91389fc729c2a1d5fb1988e065da6420a7b
Babylon <xbettybabylonx@gmail.com>
2019-04-08 00:52:16 -0700

    add and remove features working

--- index.js
+++ index.js
@@ -7,21 +7,61 @@
 //         alert("Doesn't Work");
 //     }
 // }
-$("#js-shopping-list-form").on('submit', function(e){
-	var text = $('#shopping-list-entry').val();
-  $('shopping-list').append($('<li>').append(text));
-  $('#shopping-list-entry').val('');
-  e.preventDefault();
+
+$(document).ready(function() {
+    $("#js-shopping-list-form").on('submit', function(e){
+      	let text = $('#shopping-list-entry').val();
+        let buttonLabelOpen ='<span class="shopping-item">'
+        let buttonLabelClose = '</span>';
+        let itemControls= '<div class="shopping-item-controls">'
+            + '<button class="shopping-item-toggle">'
+            + '<span class="button-label">check</span>'
+            + '</button>'
+            + '<button class="shopping-item-delete">'
+            + '<span class="button-label">delete</span>'
+            + '</button> </div>';
+
+        $('.shopping-list').append('<li>' + buttonLabelOpen + text + buttonLabelClose + itemControls + '</li>');
+
+        // $('#shopping-list-entry').val('');
+        e.preventDefault();
+    });
+
+    $('.shopping-item-delete').on('click', function(e){
+        $(this).parent().parent().remove();
+    });
+    $('.shopping-item-toggle').on('click', function(e){
+        $('.shopping-item-toggle').closest('span').toggleClass();
+
+    })
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // $("shopping-list").on('submit', function () {
 // var text = $("addtolist").val();
 // $("#shopping-list-entry").append('<li>' + text + '</li>');
 //   e.preventDefault();
-
-}};
-
 
 
 //
